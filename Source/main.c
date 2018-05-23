01e39ddd9bcd451860dd40478c331ee710a243db
Dai Nguyen <dai@localhost>
2018-05-23 17:10:35 -0400

    Motor can be connected to  J8 of the STC  Eval Board (need to be careful with the RED (V+) wire of the Motor

--- Source/main.c
+++ Source/main.c
@@ -22,21 +22,22 @@ void main (void)  {
   unsigned char j;                    /* LED var */
 	
 	// One revolution CCW using full step mode
-	for (cycle = 0; cycle < 512*4; cycle++)
+	//for (cycle = 0; cycle < 512*4; cycle++)
+	while(1)
 	{
-			P0=0x13;
+			P2=0x13;
 			for (i = 0; i < 2000; i++)  {  /* Delay for 10000 Counts */
 				wait ();                       /* call wait function */
       }
-			P0=0x26;
+			P2=0x26;
 			for (i = 0; i < 2000; i++)  {  /* Delay for 10000 Counts */
 				wait ();                       /* call wait function */
       }
-			P0=0x4C;
+			P2=0x4C;
 			for (i = 0; i < 2000; i++)  {  /* Delay for 10000 Counts */
 				wait ();                      /* call wait function */
       }
-			P0=0x89;
+			P2=0x89;
 			for (i = 0; i < 2000; i++)  {  /* Delay for 10000 Counts */
 				wait ();                       /* call wait function */
       }
